873fe17cadc76ddcf53ca8596e658919e8997c18
Daniel Rodriguez <dRodriguez@andigital.com>
2017-10-08 17:58:16 +0100

    Added support for Ionic view Cloud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -15,6 +16,12 @@ import { CallsListProvider } from '../providers/calls-list/calls-list';
 
 import { OrderByNumberPipe } from '../pipes/order-by-number/order-by-number';
 
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '5596f201'
+  }
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -26,7 +33,8 @@ import { OrderByNumberPipe } from '../pipes/order-by-number/order-by-number';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
